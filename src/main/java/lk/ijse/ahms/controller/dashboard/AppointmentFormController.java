f380f94ab6940170fe562492dbdbc09099d39057
Sameera Madushan Silva <madushansameera499@gmail.com>
2023-11-03 09:52:45 +0530

    sql quarry added

--- src/main/java/lk/ijse/ahms/controller/dashboard/AppointmentFormController.java
+++ src/main/java/lk/ijse/ahms/controller/dashboard/AppointmentFormController.java
@@ -1,4 +1,22 @@
 package lk.ijse.ahms.controller.dashboard;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AppointmentFormController {
+
+    public void makeApointmentOnAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/add/addApointment_form.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+
+    }
 }
